2310b77a971c04930fed11ccaabe4862113646f7
rwood219 <58787084+rwood219@users.noreply.github.com>
2020-09-18 20:12:35 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,50 @@
 window.addEventListener("load", () => {
-  getSearch = () => {
-    let searchText = document.getElementById("search").value;
-    drinkName = searchText;
-    fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`
-    ).then(function (response) {
-      if (response.status !== 200) {
-        console.log("problem" + response.status);
+  // getSearch = () => {
+  let searchText = document.getElementById("search").value;
+  drinkName = "rum";
+  //drinkName = 'searchText'
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`
+  ).then(function (response) {
+    if (response.status !== 200) {
+      console.log("problem" + response.status);
+    }
+    response.json().then(function (data) {
+      console.log(data);
+      let searchedDrink = data.drinks;
+      for (let i = 0; i < searchedDrink.length; i++) {
+        let drinkName = searchedDrink[i].strDrink;
+        let drinkIng = searchedDrink[i].strIngredient2;
+        let ing0 = searchedDrink[i].strIngredient0;
+        let ing1 = searchedDrink[i].strIngredient1;
+        let ing2 = searchedDrink[i].strIngredient2;
+        let ing3 = searchedDrink[i].strIngredient3;
+        let msr1 = searchedDrink[i].strMeasure1;
+        let msr2 = searchedDrink[i].strMeasure2;
+        let msr3 = searchedDrink[i].strMeasure3;
+        console.log(msr1);
+
+        createListItem = (text) => {
+          let li = document.createElement("li");
+          li.textContent = text;
+          return li;
+        };
+
+        const space = "---------------------";
+        //set ui stuff
+        const recipe = document.getElementById("recipe");
+        recipe.appendChild(createListItem(drinkName));
+        recipe.appendChild(createListItem(drinkIng));
+        recipe.appendChild(createListItem(ing0));
+        recipe.appendChild(createListItem(ing1 + " " + msr1));
+        recipe.appendChild(createListItem(ing2 + " " + msr2));
+        recipe.appendChild(createListItem(ing3 + " " + msr3));
+        recipe.appendChild(createListItem(searchedDrink.strIngredient4));
+        recipe.appendChild(createListItem(searchedDrink.strIngredient5));
+        recipe.appendChild(createListItem(searchedDrink.strIngredient6));
+        recipe.appendChild(createListItem(space));
       }
-      response.json().then(function (data) {
-        let searchedDrink = data.drinks;
-        for (let i = 0; i < searchedDrink.length; i++) {
-          let drinkName = searchedDrink[i].strDrink;
-          let drinkIng = searchedDrink[i].strIngredient2;
-          let ing0 = searchedDrink[i].strIngredient0;
-          let ing1 = searchedDrink[i].strIngredient1;
-          let ing2 = searchedDrink[i].strIngredient2;
-          let ing3 = searchedDrink[i].strIngredient3;
-
-          createListItem = (text) => {
-            let li = document.createElement("li");
-            li.textContent = text;
-            return li;
-          };
-
-          const space = "---------------------";
-          //set ui stuff
-          const recipe = document.getElementById("recipe");
-          recipe.appendChild(createListItem(drinkName));
-          recipe.appendChild(createListItem(drinkIng));
-          recipe.appendChild(createListItem(ing0));
-          recipe.appendChild(createListItem(ing1));
-          recipe.appendChild(createListItem(ing2));
-          recipe.appendChild(createListItem(ing3));
-          recipe.appendChild(createListItem(searchedDrink.strIngredient4));
-          recipe.appendChild(createListItem(searchedDrink.strIngredient5));
-          recipe.appendChild(createListItem(searchedDrink.strIngredient6));
-          recipe.appendChild(createListItem(space));
-        }
-      });
     });
-  };
+  });
+  //  };
 });
