65220e836f26e69404bc8f545fbff93e1b72955e
rwood219 <58787084+rwood219@users.noreply.github.com>
2020-09-25 01:06:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 window.addEventListener("load", () => {
   const randomURL = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
   const searchURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`;
-  const displayRandomDrink = document.querySelector("#suggestions");
-  const randomInstructionText = document.querySelector("#instruction-text");
-  let clearInstructions = document.querySelector("#instruction-text");
-  let picClick = document.querySelector(".thumb-nail-images").children;
+  const picClick = document.querySelector(".thumb-nail-images").children;
 
   createListItem = (text) => {
     let li = document.createElement("li");
     li.textContent = text;
+    li.addEventListener("click", () => {
+      console.log("clicked");
+    });
     return li;
   };
 
@@ -17,7 +17,7 @@ window.addEventListener("load", () => {
     picClick[i].addEventListener("click", () => {
       fetchData((urlParam = picClick[i].textContent));
     });
-  }
+  };
 
   getSearch = () => {
     fetchData((urlParam = document.getElementById("search").value));
@@ -33,43 +33,51 @@ window.addEventListener("load", () => {
           let currrentDrinkSearch = data.drinks[i];
           const ing1 = currrentDrinkSearch.strIngredient1;
           const ing2 = currrentDrinkSearch.strIngredient2;
+
           let ing3 = currrentDrinkSearch.strIngredient3;
-          ing3 === null ? ing3 = '' : ing3 = ing3;
+          ing3 === null ? (ing3 = "") : (ing3 = ing3);
           let ing4 = currrentDrinkSearch.strIngredient4;
-          ing4 == null ? ing4 = '' : ing4 = ing4;
+          ing4 == null ? (ing4 = "") : (ing4 = ing4);
           let ing5 = currrentDrinkSearch.strIngredient5;
-          ing5 == null ? ing5 = '' : ing5 = ing5;
+          ing5 == null ? (ing5 = "") : (ing5 = ing5);
           let ing6 = currrentDrinkSearch.strIngredient6;
-          ing6 == null ? ing6 = '' : ing6 = ing6;
+          ing6 == null ? (ing6 = "") : (ing6 = ing6);
           let ing7 = currrentDrinkSearch.strIngredient7;
-          ing7 == null ? ing7 = '' : ing7 = ing7;
+          ing7 == null ? (ing7 = "") : (ing7 = ing7);
           let ing8 = currrentDrinkSearch.strIngredient8;
-          ing8 == null ? ing8 = '' : ing8 = ing8;
+          ing8 == null ? (ing8 = "") : (ing8 = ing8);
           let ing9 = currrentDrinkSearch.strIngredient9;
-          ing9 == null ? ing9 = '' : ing9 = ing9;
+          ing9 == null ? (ing9 = "") : (ing9 = ing9);
           let ing10 = currrentDrinkSearch.strIngredient10;
-          ing10 == null ? ing10 = '' : ing10 = ing10;
-          
+          ing10 == null ? (ing10 = "") : (ing10 = ing10);
+          let ing11 = currrentDrinkSearch.strIngredient11;
+          ing11 == null ? (ing11 = "") : (ing11 = ing11);
+          let ing13 = currrentDrinkSearch.strIngredient13;
+          ing13 == null ? (ing13 = "") : (ing13 = ing13);
+          let ing14 = currrentDrinkSearch.strIngredient14;
+          ing14 == null ? (ing14 = "") : (ing14 = ing14);
+
+          //Measure vars
           let msr1 = currrentDrinkSearch.strMeasure1;
-          msr1 == null ? msr1 = '' : msr1 = msr1;
+          msr1 == null ? (msr1 = "") : (msr1 = msr1);
           let msr2 = currrentDrinkSearch.strMeasure2;
-          msr2 == null ? msr2 = '' : msr2 = msr2;
+          msr2 == null ? (msr2 = "") : (msr2 = msr2);
           let msr3 = currrentDrinkSearch.strMeasure3;
-          msr3 == null ? msr3 = '' : msr3 = msr3;
+          msr3 == null ? (msr3 = "") : (msr3 = msr3);
           let msr4 = currrentDrinkSearch.strMeasure4;
-          msr4 == null ? msr4 = '' : msr4 = msr4;
+          msr4 == null ? (msr4 = "") : (msr4 = msr4);
           let msr5 = currrentDrinkSearch.strMeasure5;
-          msr5 == null ? msr5 = '' : msr5 = msr5;
+          msr5 == null ? (msr5 = "") : (msr5 = msr5);
           let msr6 = currrentDrinkSearch.strMeasure6;
-          msr6 == null ? msr6 = '' : msr6 = msr6;
+          msr6 == null ? (msr6 = "") : (msr6 = msr6);
           let msr7 = currrentDrinkSearch.strMeasure7;
-          msr7 == null ? msr7 = '' : msr7 = msr7;
+          msr7 == null ? (msr7 = "") : (msr7 = msr7);
           let msr8 = currrentDrinkSearch.strMeasure8;
-          msr8 == null ? msr8 = '' : msr8 = msr8;
+          msr8 == null ? (msr8 = "") : (msr8 = msr8);
           let msr9 = currrentDrinkSearch.strMeasure9;
-          msr9 == null ? msr9 = '' : msr9 = msr9;
+          msr9 == null ? (msr9 = "") : (msr9 = msr9);
           let msr10 = currrentDrinkSearch.strMeasure10;
-          msr10 == null ? msr10 = '' : msr10 = msr10;
+          msr10 == null ? (msr10 = "") : (msr10 = msr10);
 
           recipe.appendChild(createListItem(currrentDrinkSearch.strDrink));
           recipe.appendChild(document.createElement("br"));
@@ -83,6 +91,7 @@ window.addEventListener("load", () => {
           recipe.appendChild(createListItem(msr8 + " " + ing8));
           recipe.appendChild(createListItem(msr9 + " " + ing9));
           recipe.appendChild(createListItem(msr10 + " " + ing10));
+          recipe.appendChild(document.createElement("br"));
           recipe.appendChild(document.createElement("br"));
         }
       });
