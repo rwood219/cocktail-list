bee738884e7ea8a119eb164b05bafb6eb505769c
rwood219 <58787084+rwood219@users.noreply.github.com>
2020-09-25 18:27:51 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -12,12 +12,18 @@ window.addEventListener("load", () => {
     return li;
   };
 
+  setValue = (val1, val2) => {
+    val1 == null ? (val1 = "") : (val1 = val1);
+    val2 == null ? (val2 = "") : (val2 = val2);
+    return val1 + " " + val2;
+  };
+
   //ADD CLICK LISTENER TO PIC ELEMENTS AND SET fetch param
   for (let i = 0; i < picClick.length; i++) {
     picClick[i].addEventListener("click", () => {
       fetchData((urlParam = picClick[i].textContent));
     });
-  };
+  }
 
   getSearch = () => {
     fetchData((urlParam = document.getElementById("search").value));
@@ -31,66 +37,135 @@ window.addEventListener("load", () => {
 
         for (let i = 0; i < data.drinks.length; i++) {
           let currrentDrinkSearch = data.drinks[i];
-          const ing1 = currrentDrinkSearch.strIngredient1;
-          const ing2 = currrentDrinkSearch.strIngredient2;
-
-          let ing3 = currrentDrinkSearch.strIngredient3;
-          ing3 === null ? (ing3 = "") : (ing3 = ing3);
-          let ing4 = currrentDrinkSearch.strIngredient4;
-          ing4 == null ? (ing4 = "") : (ing4 = ing4);
-          let ing5 = currrentDrinkSearch.strIngredient5;
-          ing5 == null ? (ing5 = "") : (ing5 = ing5);
-          let ing6 = currrentDrinkSearch.strIngredient6;
-          ing6 == null ? (ing6 = "") : (ing6 = ing6);
-          let ing7 = currrentDrinkSearch.strIngredient7;
-          ing7 == null ? (ing7 = "") : (ing7 = ing7);
-          let ing8 = currrentDrinkSearch.strIngredient8;
-          ing8 == null ? (ing8 = "") : (ing8 = ing8);
-          let ing9 = currrentDrinkSearch.strIngredient9;
-          ing9 == null ? (ing9 = "") : (ing9 = ing9);
-          let ing10 = currrentDrinkSearch.strIngredient10;
-          ing10 == null ? (ing10 = "") : (ing10 = ing10);
-          let ing11 = currrentDrinkSearch.strIngredient11;
-          ing11 == null ? (ing11 = "") : (ing11 = ing11);
-          let ing13 = currrentDrinkSearch.strIngredient13;
-          ing13 == null ? (ing13 = "") : (ing13 = ing13);
-          let ing14 = currrentDrinkSearch.strIngredient14;
-          ing14 == null ? (ing14 = "") : (ing14 = ing14);
-
-          //Measure vars
-          let msr1 = currrentDrinkSearch.strMeasure1;
-          msr1 == null ? (msr1 = "") : (msr1 = msr1);
-          let msr2 = currrentDrinkSearch.strMeasure2;
-          msr2 == null ? (msr2 = "") : (msr2 = msr2);
-          let msr3 = currrentDrinkSearch.strMeasure3;
-          msr3 == null ? (msr3 = "") : (msr3 = msr3);
-          let msr4 = currrentDrinkSearch.strMeasure4;
-          msr4 == null ? (msr4 = "") : (msr4 = msr4);
-          let msr5 = currrentDrinkSearch.strMeasure5;
-          msr5 == null ? (msr5 = "") : (msr5 = msr5);
-          let msr6 = currrentDrinkSearch.strMeasure6;
-          msr6 == null ? (msr6 = "") : (msr6 = msr6);
-          let msr7 = currrentDrinkSearch.strMeasure7;
-          msr7 == null ? (msr7 = "") : (msr7 = msr7);
-          let msr8 = currrentDrinkSearch.strMeasure8;
-          msr8 == null ? (msr8 = "") : (msr8 = msr8);
-          let msr9 = currrentDrinkSearch.strMeasure9;
-          msr9 == null ? (msr9 = "") : (msr9 = msr9);
-          let msr10 = currrentDrinkSearch.strMeasure10;
-          msr10 == null ? (msr10 = "") : (msr10 = msr10);
 
           recipe.appendChild(createListItem(currrentDrinkSearch.strDrink));
           recipe.appendChild(document.createElement("br"));
-          recipe.appendChild(createListItem(msr1 + " " + ing1));
-          recipe.appendChild(createListItem(msr2 + " " + ing2));
-          recipe.appendChild(createListItem(msr3 + " " + ing3));
-          recipe.appendChild(createListItem(msr4 + " " + ing4));
-          recipe.appendChild(createListItem(msr5 + " " + ing5));
-          recipe.appendChild(createListItem(msr6 + " " + ing6));
-          recipe.appendChild(createListItem(msr7 + " " + ing7));
-          recipe.appendChild(createListItem(msr8 + " " + ing8));
-          recipe.appendChild(createListItem(msr9 + " " + ing9));
-          recipe.appendChild(createListItem(msr10 + " " + ing10));
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient1,
+                currrentDrinkSearch.strMeasure1
+              )
+            )
+          );
+
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient2,
+                currrentDrinkSearch.strMeasure2
+              )
+            )
+          );
+
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient3,
+                currrentDrinkSearch.strMeasure3
+              )
+            )
+          );
+
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient4,
+                currrentDrinkSearch.strMeasure4
+              )
+            )
+          );
+
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient5,
+                currrentDrinkSearch.strMeasure5
+              )
+            )
+          );
+
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient6,
+                currrentDrinkSearch.strMeasure6
+              )
+            )
+          );
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient7,
+                currrentDrinkSearch.strMeasure7
+              )
+            )
+          );
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient8,
+                currrentDrinkSearch.strMeasure8
+              )
+            )
+          );
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient9,
+                currrentDrinkSearch.strMeasure9
+              )
+            )
+          );
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient10,
+                currrentDrinkSearch.strMeasure10
+              )
+            )
+          );
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredien11,
+                currrentDrinkSearch.strMeasure11
+              )
+            )
+          );
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient12,
+                currrentDrinkSearch.strMeasure12
+              )
+            )
+          );
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient13,
+                currrentDrinkSearch.strMeasure13
+              )
+            )
+          );
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient14,
+                currrentDrinkSearch.strMeasure14
+              )
+            )
+          );
+          recipe.appendChild(
+            createListItem(
+              setValue(
+                currrentDrinkSearch.strIngredient15,
+                currrentDrinkSearch.strMeasure15
+              )
+            )
+          );
+
           recipe.appendChild(document.createElement("br"));
           recipe.appendChild(document.createElement("br"));
         }
